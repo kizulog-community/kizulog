309ed5fe098cc0bd37a3cc70e519eca0bb115974
Jun Kobayashi <b5ypxzae@gmail.com>
2026-04-24 02:54:41 +0900

    feat: initial Spring Boot 4.0.6 project setup

---
+++ src/test/java/io/github/kizulog_community/kizulog/KizulogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kizulog_community.kizulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KizulogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
