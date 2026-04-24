db9e7406af8e43f99c4e68a9e77b870d9c90a057
Jun Kobayashi <b5ypxzae@gmail.com>
2026-04-24 15:06:13 +0900

    fix(config): fix flyway and security configuration for Spring Boot 4

--- src/main/java/io/github/kizulog_community/kizulog/KizulogApplication.java
+++ src/main/java/io/github/kizulog_community/kizulog/KizulogApplication.java
@@ -2,8 +2,15 @@ package io.github.kizulog_community.kizulog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    SecurityAutoConfiguration.class,
+	    SecurityFilterAutoConfiguration.class,
+	    UserDetailsServiceAutoConfiguration.class
+	})
 public class KizulogApplication {
 
 	public static void main(String[] args) {
